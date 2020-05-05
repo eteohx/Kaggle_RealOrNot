732069e15440e9156e0679950a60eead78a9e2b9
Emily <42155137+eteohx@users.noreply.github.com>
2020-05-05 11:31:45 +0100

    Update real_or_not.py

--- real_or_not.py
+++ real_or_not.py
@@ -1,55 +1,86 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-#load pre-trained glove vectors
-#embeddings_dict = {}
-#with open("G:/My Drive/DublinAI/Mini Projects/nlp-getting-started/glove.6B/glove.6B.50d.txt", 'r', encoding="utf-8") as f:
-#    for line in f:
-#        values = line.split()
-#        word = values[0]
-#        vector = np.asarray(values[1:], "float32")
-#        embeddings_dict[word] = vector
-    
 #load data
 import pandas as pd
 
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
 
-#tokenize data
-#train.text = train.text.apply(lambda x: nltk.regexp_tokenize(x.lower(), r'\w+'))
-#test.text = test.text.apply(lambda x: nltk.regexp_tokenize(x.lower(), r'\w+'))
+# PRE-PROCESSING
+import nltk
+from nltk.corpus import stopwords
+print('before: ' + train.text[7])
+print('before: ' + train.text[8])
+print('before: ' + train.text[15])
 
 # remove stopwords
-#train.text=[w for w in train.text if w not in stopwords.words('english')]
-#test.text=[w for w in test.text if w not in stopwords.words('english')]
+for i in stopwords.words('english'):
+    train = train.replace(to_replace=r'\b%s\b\s'%i, value=" ",regex=True)
+    test = train.replace(to_replace=r'\b%s\b\s'%i, value=" ",regex=True)
+print('after stopwords: ' + train.text[7])
+print('after stopwords: ' + train.text[8])
+print('after stopwords: ' + train.text[15])
 
-#get average glove vector
+# remove hyperlinks
+train = train.replace(to_replace=r'http\S+', value="", regex=True)
+test = test.replace(to_replace=r'http\S+', value="", regex=True)
+print('after hyperlinks: ' + train.text[7])
+print('after HL: ' + train.text[8])
+print('after HL: ' + train.text[15])
+
+# remove mentions
+train = train.replace(to_replace=r'@\S+', value="", regex=True)
+test = test.replace(to_replace=r'@\S+', value="", regex=True)
+train=train.replace(to_replace=r'Err:\S+', value='error ', regex=True)
+test=test.replace(to_replace=r'Err:\S+', value='error ', regex=True)
+print('after mentions: ' + train.text[7])
+print('after m: ' + train.text[8])
+print('after m: ' + train.text[15])
+
+#replace punctuation
+train = train.replace(to_replace=r'\...', value=' ', regex=True)
+test = test.replace(to_replace=r'\...', value=' ', regex=True)
+train = train.replace(to_replace=r'[\'\^\\,@\‘?!\.$%_:\-“’“”\#\/\*]', value='', regex=True)
+test = test.replace(to_replace=r'[\'\^\\,@\‘?!\.$%_:\-“’“”\#\/\*]', value='', regex=True)
+print('after punctuation: ' + train.text[7])
+print('after p: ' + train.text[8])
+print('after p: ' + train.text[15])
+
+#make lowercase
+train.text = train.text.apply(lambda x: x.lower())
+test.text = test.text.apply(lambda x: x.lower())
+
+#replace spaces
+train=train.replace(to_replace='\n', value=' ',regex=True)
+test=test.replace(to_replace='\n', value=' ',regex=True)
+train = train.replace(to_replace=r'    ', value=' ', regex=True)
+test = test.replace(to_replace=r'    ', value=' ', regex=True)
+train = train.replace(to_replace=r'   ', value=' ', regex=True)
+test = test.replace(to_replace=r'   ', value=' ', regex=True)
+train = train.replace(to_replace=r'  ', value=' ', regex=True)
+test = test.replace(to_replace=r'  ', value=' ', regex=True)
+print('after spaces: ' + train.text[7])
+print('after s: ' + train.text[8])
+print('after s: ' + train.text[15])
+
+#GET AVERAGE GLOVE VECTOR
 import spacy
 import numpy as np
 import re 
 nlp = spacy.load("en_core_web_md")
 
-train.text = train.text.apply(lambda x: ' '.join(re.findall("[a-zA-Z!]+", x)))
 train_feature = np.array([nlp(x.lower()).vector for x in train.text])
-
 # add keyword if available
 #train.keyword = train.keyword.apply(lambda x: x if pd.isnull(x) else ' '.join(re.findall("[a-zA-Z!]+", x)))
 #for i in range(len(train_feature)):
 #    if not pd.isnull(train.keyword[i]):
 #        train_feature[i] = (train_feature[i]+nlp(train.keyword[i].lower()).vector)/2   
 
-test.text = test.text.apply(lambda x: ' '.join(re.findall("[a-zA-Z!]+", x)))
 test_feature = np.array([nlp(x.lower()).vector for x in test.text])
 #test.keyword = test.keyword.apply(lambda x: x if pd.isnull(x) else ' '.join(re.findall("[a-zA-Z!]+", x)))
 #for i in range(len(test_feature)):
 #    if not pd.isnull(test.keyword[i]):
 #        test_feature[i] = (test_feature[i]+nlp(test.keyword[i].lower()).vector)/2   
 
+# TRAIN SVM
 from sklearn.model_selection import cross_val_score
 from sklearn import svm
 clf = svm.SVC()
@@ -60,21 +91,4 @@ clf = svm.SVC().fit(train_feature, train.target)
 pred = pd.DataFrame({'id':test.id,'target': clf.predict(test_feature)})
 pred.to_csv('submission.csv',index=False)
 
-# visualise data
-# real_train = train[train.target==1]
-# not_real_train = train[train.target==0]
-
-# a = np.array([i for i in set(not_real_train.keyword)])
-# b = np.array([sum(not_real_train.keyword == i) for i in a])
-# c = np.argsort(-1*b)
-
-# plt.rcdefaults()
-# fig, ax = plt.subplots()
-# y_pos = np.arange(10)
-
-# ax.barh(y_pos, b[c[0:10]], align='center')
-# ax.set_yticks(y_pos)
-# ax.set_yticklabels(a[c[0:10]])
-# ax.invert_yaxis()
-# plt.show()
 
